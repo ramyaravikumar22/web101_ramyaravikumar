9a88be88be3f3071cae8233c9c91f8a48957dab7
ramyaravikumar22 <ramya38br@gmail.com>
2022-07-31 14:05:01 -0500

    saving progress

--- portfolio/Portfolio1/contact/scripts.js
+++ portfolio/Portfolio1/contact/scripts.js
@@ -1,13 +1,13 @@
 function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-//	Username : "Your Gmail Address",
-//	Password : "Your Gmail Password",
-	To : 'smrithibr.br@gmail.com',
-	From : "ramya38br@gmail.com",
-	Subject : "email subject",
-	Body : "email body",
-	}).then(
-		message => alert("mail sent successfully")
-	);
-}
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "smrithibr.br@gmail.com",
+        Password : "75260AF7C1C8F1F9A8ECFE000394D9C005D7",
+        To : 'smrithi.ravikumar1993@gmail.com',
+        From : "smrithibr.br@gmail.com",
+        Subject : "Product manager interview",
+        Body : "<html><h2>Hi Smrithi</h2><br.<strong>I hope you are having a great day.</strong><br></br><em>Italic</em></html>"
+    }).then(
+      alert("Email sent")
+    );
+    }
