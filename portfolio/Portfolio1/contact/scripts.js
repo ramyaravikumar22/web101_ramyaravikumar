14a3f44d0d04ff3b1793d08c8e88deb2d98dc848
ramyaravikumar22 <ramya38br@gmail.com>
2022-09-13 21:23:27 -0500

    Made changes to my portfolio

--- portfolio/Portfolio1/contact/scripts.js
+++ portfolio/Portfolio1/contact/scripts.js
@@ -1,4 +1,4 @@
-function sendEmail() {
+/*function sendEmail() {
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "smrithibr.br@gmail.com",
@@ -10,4 +10,60 @@ function sendEmail() {
     }).then(
       alert("Email sent")
     );
+    }*/
+
+//get data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+    //validate data
+   function validateForm(){
+
+clearMessages ();
+let errorFlag = false;
+
+
+    if(nameInput.value.length < 1){
+      errorNodes[0].innerText = "Name cannot be blank";
+      nameInput.classList.add("error-border");
+      errorFlag = true;
     }
+
+    if(!emailIsValid(email.value)){
+      errorNodes[1].innerText = "Invalid email addess";
+      nameInput.classList.add("error-border");
+    }
+
+
+    if(message.value.length < 1){
+      errorNodes[2].innerText = "Please enter message";
+      email.classList.add("error-border");
+      errorFlag = true;
+    }
+
+    if(!errorFlag){
+      success.innerText = "Success!";
+    }
+   }
+
+   //Clear error /success messages
+
+   function clearMessages() {
+    for(let i=0; i < errorNodes.length; i++){
+      errorNodes[i].innerText = "";
+    }
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+   }
+
+   //check email validity
+
+   function emailIsValid(email){
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+   }
